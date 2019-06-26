8f0b1d664c0cb53c33b3a7b0563097f1b5709d9c
tobiolabode <tobi@tobiolabode.com>
2019-06-26 16:13:56 +0100

    extracted html body, next step extract the dates

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup
+import re
 
-soup = BeautifulSoup(open("Starting strength log.html"), "html.parser")
+file = open("Starting strength log.html", encoding="utf8")
+soup = BeautifulSoup(file, "html.parser")
+# swap to lxml if any emoji related problems occours
 
-print(soup)
+# print(soup.prettify())
+bodyhtml = soup.find("body")
+# print(bodyhtml.prettify())
+divtag = bodyhtml.find('div')
+
+print(divtag.prettify())
+
+# (.*september).\d*  to find month stings
+# use this find the individual div tag of the day
--- plan.md
+++ plan.md
@@ -1,14 +1,15 @@
 # Plan
 
-## Scrap evernote starting strength note to stats.    
+## Scrap Evernote starting strength note to stats.    
 
 1. Use HTML file
     - [x] Get html file
     - [ ]
 2. Use Beautiful Soup 4
-    - [ ] import bs4
-    - [ ] 
+    - [x] import bs4
+    - [ ] extract dates and numbers
 
 3. scrap html to raw numbers
+    - []
 4.  raw numbers to csv
 5. use pandas or google drive
