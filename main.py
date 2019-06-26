3255e589ec87ddc09b4f92c1b746db12b13786b9
tobiolabode <tobi@tobiolabode.com>
2019-06-26 19:24:42 +0100

    printing siblings content as lists.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import re
+import itertools
 
 file = open("Starting strength log.html", encoding="utf8")
 soup = BeautifulSoup(file, "html.parser")
@@ -10,19 +11,36 @@ bodyhtml = soup.find("body")
 # print(bodyhtml.prettify())
 divtag = bodyhtml.find('div')
 
-#print(divtag.prettify())
+inner_div_tag = divtag.span.div
+
+# print(divtag.prettify())
 
 # (.*september).\d*  to find month stings
 # use this find the individual div tag of the day
-#sideway navigation to other div tags contains lifts numbers
+# sideway navigation to other div tags contains lifts numbers
 
-#print(divtag.find('September'))
+# print(divtag.find('September'))
 
-#print(divtag.span.div.next_sibling)
+# print(divtag.span.div.next_sibling)
 
-for sibling in divtag.span.div.next_siblings:
-    print(sibling)
+sibling_contents = []
+for sibling in inner_div_tag.next_siblings:
+    # print(sibling)
+    sibling_contents.append(sibling.contents)
+    print(sibling.contents)
+print('\n --------------------------- \n')
+print("Results of list")
+print(sibling_contents)
 
-#sibling[10]
+sibling_contents = list(itertools.chain(*sibling_contents))
+print('\n --------------------------- \n')
+print('\n --------------------------- \n')
+print('\n --------------------------- \n')
+print(sibling_contents)
 
-#print(divtag.span.div.find_next_siblings('div'))
+
+
+
+# sibling[10]
+
+# print(divtag.span.div.find_next_siblings('div'))
