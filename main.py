0110b0bcd8280a432898655c7000402cd9ef66d6
tobiolabode <tobi@tobiolabode.com>
2019-06-26 18:32:12 +0100

    Nagivating through the tree. dates and lifts are on the same level

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ bodyhtml = soup.find("body")
 # print(bodyhtml.prettify())
 divtag = bodyhtml.find('div')
 
-print(divtag.prettify())
+#print(divtag.prettify())
 
 # (.*september).\d*  to find month stings
 # use this find the individual div tag of the day
+#sideway navigation to other div tags contains lifts numbers
+
+#print(divtag.find('September'))
+
+#print(divtag.span.div.next_sibling)
+
+for sibling in divtag.span.div.next_siblings:
+    print(sibling)
+
+#sibling[10]
+
+#print(divtag.span.div.find_next_siblings('div'))
